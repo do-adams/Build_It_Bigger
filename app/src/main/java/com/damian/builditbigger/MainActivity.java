a553928128bebc4fd7513d1bb7cd453d24977fc6
Damian Adams <reikjavic@live.com>
2016-08-03 16:41:05 -0400

    feat: Adds a test Google Ad Mob banner to the MainActivity.

--- app/src/main/java/com/damian/builditbigger/MainActivity.java
+++ app/src/main/java/com/damian/builditbigger/MainActivity.java
@@ -1,7 +1,11 @@
 package com.damian.builditbigger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(getApplicationContext(), getString(R.string.ad_mob_app_id));
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+
+        // use your own test device id (check the log)
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("7FDA1E82084FB2DA81E38E52B97C7981")
+                .build();
+        mAdView.loadAd(adRequest);
     }
+
 }
